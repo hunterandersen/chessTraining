ed864d83332393129a3266481ea2a140cc321847
Hunter Andersen <andersen.hunter42@gmail.com>
2024-08-29 22:41:44 -0700

    fixed query function

--- server/layers/dbConfig.js
+++ server/layers/dbConfig.js
@@ -1,10 +1,21 @@
 const mysql = require("mysql2");
 const config = require("./config");
 
-mysql.createPool({
+const connection = mysql.createPool({
     MYSQLDATABASE: config.MYSQLDATABASE,
     MYSQLHOST: config.MYSQLHOST,
     MYSQLPASSWORD: config.MYSQLPASSWORD,
     MYSQLPORT: config.MYSQLPORT,
     MYSQLUSER: config.MYSQLUSER,
 });
+
+function query(queryStr, values) {
+    return new Promise((resolve, reject) => {
+        connection.query(queryStr, values, (err, result) => {
+            if (err) reject(err)
+            else resolve(result);
+        })
+    });
+}
+
+module.exports = query;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const config = require("./layers/config.js");
+const query = require("./layers/dbConfig.js");
 
 const server = express();
+
+server.use(cors());
+
 
 server.listen(config.PORT, () => {
     console.log(`Server listening on port ${config.PORT}`);
